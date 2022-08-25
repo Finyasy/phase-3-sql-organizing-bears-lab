cf0af2ab7c1cb1ed6ed51c8f6b45b5435dc6bd0d
Bryan Bosire <bryanbosire@gmail.com>
2022-08-25 21:46:29 +0300

    commit tree 84b3c472de2bf9cdf81058a7f31c5172f592a6db

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (1,"Mr. Chocolate",79,"M","white","happy","False");
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (2,"Rowdy",45,"M","black","excited","True");
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (3,"Tabitha",30,"F","pink","analytical","True");
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (4,"Sergeant Brown",27,"M","brown","excited","True");
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (5,"Melissa",40, "F","brown","quiet","True");
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (6,"Grinch",60, "M","black","sad","True");
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (7,"Wendy", 26,"F","brown","relaxed","True");
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (8,NULL, 37,"M","brown","opinionated","True");
